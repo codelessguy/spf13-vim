b03e383248f1a315fa0458aafacf095ee99c6c08
codelessguy <rikroled@gmail.com>
2014-02-02 11:15:49 +0100

    [Install script] add sdl2, opengl3 and git config

--- ubuntu-installer.sh
+++ ubuntu-installer.sh
@@ -1,7 +1,33 @@
 #!/usr/bin/env bash
 
-sudo apt-get install xuberant-ctags
+sudo apt-get -y install git gitg vim vim-gnome
+
+#SDL 1.2
+#sudo apt-get -y install libsdl1.2-dev libsdl-mixer1.2-dev libsdl-image1.2-dev libsdl-ttf2.0-dev
+
+# SDL 2
+sudo apt-get -y install libsdl2-2.0-0 libsdl2-dbg libsdl2-dev
+
+# OpenGL 3
+sudo apt-get -y install freeglut3-dev libglew-dev
+
+sudo apt-get -y install xuberant-ctags
 
 # Enable terminal 256 colors
 sudo apt-get install ncurses-term
-echo "export TERM=xterm-256color" >> ~/.bashrc
+#echo "export TERM=xterm-256color" >> ~/.bashrc
+
+# Git config
+echo "[color]
+    diff = auto
+    status = auto
+    branch = auto
+[user]
+    name = codelessguy
+    email = rikroled@gmail.com
+[alias]
+    ci = commit
+    co = checkout
+    st = status
+    br = branch" > ~/.gitconfig
+
