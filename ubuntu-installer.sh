9251f471943befd84d340799718e7367f111112f
codelessguy <rikroled@gmail.com>
2014-02-02 11:33:48 +0100

    [Install script] Use a single apt-get cmd to install

--- ubuntu-installer.sh
+++ ubuntu-installer.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install git gitg vim vim-gnome
-
-#SDL 1.2
-#sudo apt-get -y install libsdl1.2-dev libsdl-mixer1.2-dev libsdl-image1.2-dev libsdl-ttf2.0-dev
-
+pkglists="git gitg vim vim-gnome exuberant-ctags"
+# SDL 1.2
+#pkglists="$pkglists libsdl1.2-dev libsdl-mixer1.2-dev libsdl-image1.2-dev libsdl-ttf2.0-dev"
 # SDL 2
-sudo apt-get -y install libsdl2-2.0-0 libsdl2-dbg libsdl2-dev
-
+pkglists="$pkglists libsdl2-2.0-0 libsdl2-dbg libsdl2-dev"
 # OpenGL 3
-sudo apt-get -y install freeglut3-dev libglew-dev
-
-sudo apt-get -y install xuberant-ctags
-
+pkglists="$pkglists freeglut3-dev libglew-dev"
 # Enable terminal 256 colors
-sudo apt-get install ncurses-term
-#echo "export TERM=xterm-256color" >> ~/.bashrc
+pkglists="$pkglists ncurses-term"
+echo "export TERM=xterm-256color" >> ~/.bashrc
+
+echo "INSTALL: $pkglists"
+sudo apt-get -y install $pkglists
 
 # Git config
 echo "[color]
@@ -30,4 +27,3 @@ echo "[color]
     co = checkout
     st = status
     br = branch" > ~/.gitconfig
-
